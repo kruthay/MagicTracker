3d215fd4a8a1722bb5b379f8538a7e9f2724eb6c
Kruthay Donapati <117230425+kruthay@users.noreply.github.com>
2023-08-25 16:24:01 -0400

    Initial Commit

---
+++ MagicTracker/MagicTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicTrackerApp.swift
+//  MagicTracker
+//
+//  Created by Kruthay Donapati on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MagicTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
